69a2e239d1e930dc79bf21a1c79232ceda2ea8af
Æ engineering <erdemayberkae@gmail.com>
2020-08-06 13:04:55 +0300

    bazı düzenlemeler

--- Site/Php/YeniIlan.php
+++ Site/Php/YeniIlan.php
@@ -48,6 +48,13 @@ if(array_key_exists('public',$_POST))
   <meta name="keywords" content="esogü staj sistemi,esogü">
   <meta name="viewport" content="widht-device-witdh,initial-scale-1.0">
    <link rel="stylesheet" type="text/css" href="../css/İlan.css">
+   <link rel="stylesheet" type="text/css" href="../css/reset.css">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
+
    <!-- Load Leaflet from CDN -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
     integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
@@ -73,73 +80,155 @@ if(array_key_exists('public',$_POST))
   </script>
     <title>BirAtilim</title>
     <link rel="stylesheet" type="text/css" href="../css/reset.css">
-  <link rel="stylesheet" type="text/css" href="../css/Home.css">
+
   </head>
 
   <body>
-    <header id="giriş">
-      <div class= "container" >
-        <div class="logo">
-    <a href="Home.php"><img title="Hakkımızda"src="../img/logo.png"></a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <div class="d-flex flex-grow-1">
+            <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
+            <a class="navbar-brand d-none d-lg-inline-block" href="#">
+                <img src="../Img/logo.png" alt="">
+            </a>
+            <a class="navbar-brand-two mx-auto d-lg-none d-inline-block" href="#">
+                <img src="//placehold.it/40?text=LOGO" alt="logo">
+            </a>
+            <div class="w-100 text-right">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
         </div>
-        <div class="menu">
-          <ul>
-            <li id="home" title="İlanlarım"><a href='MyPoster.php'>İlanlarım</a></li>
-              <li id="notify" title="Bildirimler"><a href='MyPoster.php'>Favorilerim</a></li>
-                <li id="about" title="Hakkında"><a href='MyPoster.php'>Ayarlar</a></li>
-                  <li id="about" title="Mesajlar"><a href='MyPoster.php'><img src="../img/Message.png"></a></li>
-          </ul>
+        <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+            <ul class="navbar-nav ml-auto flex-nowrap">
+                <li class="nav-item">
+                    <a href="YeniIlan.php" class="nav-link m-2 btn btn-warning nav-active">İlan Oluştur</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link m-2 btn btn-warning">Mesajlar</a>
+                </li>
+                <li class="nav-item">
+                    <a href="MyPage.php?user=<?php echo$_SESSION['user'];?>" class="nav-link m-2 btn btn-warning">Ayarlarım</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link m-2 btn btn-warning">Bize Yazın</a>
+                </li>
+            </ul>
         </div>
+        <form class="navbar-form"  method="post">
+      <div class="input-group">
+          <input type="text" class="form-control" placeholder="İlan ara(Id veya Tür araması)" name="search">
+          <div class="input-group-btn">
+              <input type="submit" name='Ara' value="Ara" class="glyphicon glyphicon-search btn btn-primary">
+
+          </div>
       </div>
-    </header>
-    <div class="googlearea" border="1px" height="200px" >
-          <center><h1>Select Your Location</h1></center>
+
+      </form>
+    </nav>
+    <div class="flex">
+
+
+
     <div id='map'class="map" >
 
     </div>
-    </div>
 
-    <form method="POST" name="ayberk" action="YeniIlan.php" enctype="multipart/form-data">
-<br><br>
-<label>Yer İsmi</label>
-    <input type="text" id="4"name="City" value="" placeholder="İşaret İsmi...">
-      <label>City</label>
-          <input type="text" id="0"name="City" value="" placeholder="İl...">
-          <label>District</label>
-          <input type="text" id="1"name="District" value=""placeholder="İlçe...">
-          <br>  <br>
-          <label>Address</label>
-          <textarea  id="2" name="address" rows="4" cols=30></textarea>
-
-          <label>Posta Kodu</label>
-          <input type="text" id="3"name="postcode" value=""placeholder="Açık Adres...">
-          <br>
-          <br><br><br>
-          <div>
-            <label>Resim Yükleyiniz</label>
-            <br>
-      	  <input type="file" name="image" accept="image/jpeg">
+<div class="">
+  <div class="text-center">
+  <div id="choose-form" style="text-align:right" class="btn-group"  aria-label="Basic example">
+  <button value="divadres"onclick="f1(this)"  type="button" class="btn btn-secondary">Adres</button>
+  <button value="divilan" onclick="f1(this)" type="button" class="btn btn-secondary">İlan İçeriği</button>
+</div>
+</div>
+<div>
+  <form id='adres' method="POST" name="ayberk" action="YeniIlan.php" enctype="multipart/form-data" class="well form-horizontal">
+    <div id="collapse1" class="collapse show">
+<div class="card-body" id="divadres">
+ <div class="row">
+     <div class="col-md-3 col-lg-4">
+         <div class="form-group">
+             <label class="control-label">Yer ismi</label>
+            <input class="form-control"  type="text" id="4"name="yerismi" value="" placeholder="İşaret İsmi...">
+         </div>
+     </div>
+     <div class="col-md-1 col-lg-4">
+         <div class="form-group">
+             <label class="control-label">Şehir</label>
+          <input type="text" id="0"name="City"  class="form-control"  value="" placeholder="İl...">
+         </div>
+     </div>
+     <div class="col-md-1 col-lg-4">
+         <div class="form-group">
+             <label class="control-label">İlçe</label>
+            <input class="form-control"  type="text" id="1"name="District" value=""placeholder="İlçe...">
+         </div>
+     </div>
+      </div>
+    <div class="row">
+      <div class="col-md-3 col-lg-4">
+          <div class="form-group">
+              <label class="control-label">Adres</label>
+              <textarea  class="form-control"  style="resize:none" class="form-control" id="2" name="address" rows="4" cols=30></textarea>
           </div>
-          <br>
-          <br>
-          <textarea
-          name="description"
-          	id="description"
-          	cols="40"
-          	rows="4"
-          	name="image_text"
-          	placeholder="İlanı Anlatınız..."></textarea>
-            <input type="hidden" id='5' name="latitude" value="">
-            <input type="hidden" id='6' name="longitude" value="">
-      		<input type="submit" name="public" value="Devam Et">
-          		<input type="submit" name="konum" value="konum bak">
+      </div>
+      <div class="col-md-1 col-lg-4">
+          <div class="form-group">
+              <label class="control-label">Posta Kodu</label>
+              <input class="form-control"  type="text" id="3"name="postcode" value=""placeholder="Açık Adres...">
+          </div>
+      </div>
+      </div>
+      <div class="row">
 
-      </form>
-      <script type="text/javascript">
+ </div>
+     </div>
+
+       <input type="hidden" id='5' name="latitude" value="">
+       <input type="hidden" id='6' name="longitude" value="">
+     </form>
+      <div class="card-body" id="divilan">
+ <form id='ilan' method="POST" name="ayberk" action="YeniIlan.php" enctype="multipart/form-data" class="well form-horizontal">
+<div class="row">
+    <input type="file" name="image" accept="image/jpeg">
+
+   <div class="col-md-1 col-lg-4">
+       <div class="form-group">
+ <textarea
+  class="form-control"
+ name="description"
+   id="description"
+   cols="35"
+   rows="5"
+   name="image_text"
+   style="resize:none"
+   placeholder="İlanı Anlatınız..."></textarea>
+ </div>
+</div>
+   <div class="col-md-1 col-lg-4">
+       <div class="form-group">
+           <label class="text-center">Yayınla</label>
+           <input class="form-control" type="submit" name="public" value="Devam Et">
+       </div>
+     </div>
+  </form>
+ </div>
+ </div>
+</div>
+<script type="text/javascript">
+$("#divilan").hide();
+function f1(objButton){
+
+            $("#divadres, #divilan").hide();
+
+            $("#" + $(objButton).val()).show();
+  }
+</script>
+<script type="text/javascript">
       function initMap() {
 
       var infoWindow = new google.maps.InfoWindow;
-var warning=0;
+      var warning=0;
         // Change this depending on the name of your PHP or XML file
 
         if (navigator.geolocation) {
